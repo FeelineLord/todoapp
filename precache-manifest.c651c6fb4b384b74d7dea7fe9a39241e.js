e77a4bf8b48682f3a5fdce516af2f02bda25fa0a
FeelineLord <feelinelord@gmail.com>
2020-02-23 19:05:44 +0200

    Updates

---
+++ precache-manifest.c651c6fb4b384b74d7dea7fe9a39241e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40773992fb135c2fc86e3a717cf75a0",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "519419433f187ea77030",
+    "url": "/todoapp/static/css/main.ff55e328.chunk.css"
+  },
+  {
+    "revision": "42ed9f85815ac885170f",
+    "url": "/todoapp/static/js/2.3fb10a54.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/todoapp/static/js/2.3fb10a54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "519419433f187ea77030",
+    "url": "/todoapp/static/js/main.06e1e7f8.chunk.js"
+  },
+  {
+    "revision": "6b2e16d69d651fe42878",
+    "url": "/todoapp/static/js/runtime-main.37b5055a.js"
+  }
+]);
